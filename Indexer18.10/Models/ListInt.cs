66894118ac83b7f1c62ee6bc304e07fea6fff14d
Shamama Guliyeva <shamamaguliyeva04@gmail.com>
2023-10-17 23:02:34 +0400

    Task added

---
+++ Indexer18.10/Models/ListInt.cs
@@ -0,0 +1,104 @@
+﻿using System;
+namespace Indexer18._10.Models
+{
+	internal class ListInt
+	{
+		private int[] _array;
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < _array.Length)
+                {
+                    return _array[index];
+                }
+                return _array[_array.Length - 1];
+
+
+            }
+            set
+            {
+                if (index < _array.Length)
+                {
+                    _array[index] = value;
+                }
+
+            }
+        }
+
+        public ListInt()
+        {
+            _array = new int[0];
+        }
+
+
+        public void Add(int num)
+        {
+            Array.Resize(ref _array, _array.Length + 1);
+            _array[_array.Length - 1] = num;
+        }
+
+        public void AddRange(params int[] nums)
+        {
+            Array.Resize(ref _array, _array.Length + nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                _array[(_array.Length - nums.Length) + i] = nums[i];
+            }
+        }
+
+        public bool Contains(int num)
+        {
+            foreach (var item in _array)
+            {
+                if (item == num)
+                    return true;
+            }
+            return false;
+        }
+
+        public int Sum()  //Muellim ozum hazir method istifade etmedim ki, birden bal kesersiz :D
+        {
+            int sum = 0;
+            foreach (var item in _array)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        public void Remove(int num) 
+        {
+            int[] _newarray = new int[0];
+            foreach (int i in _array)
+            {
+                if (i!=num)
+                {
+                    Array.Resize(ref _newarray, _newarray.Length + 1);
+                    _newarray[^1] = i;
+                }
+            }
+            _array = _newarray;
+        }
+
+        public void RemoveRange(params int[] nums)
+        {
+
+            foreach (int i in nums)
+            {
+                Remove(i);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", _array);
+        }
+
+    }
+
+
+}
+
---
+++ Indexer18.10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Indexer18._10.Models;
+
+namespace Indexer18._10;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ListInt array = new ListInt();
+        array.Add(3);
+        array.Add(6);
+
+        array.AddRange(12, 15, 30, 12, 7, 9, 10, 3, 9);
+
+        Console.WriteLine("Array: " + array.ToString());
+        Console.WriteLine("Contains: " + array.Contains(10));
+        Console.WriteLine("Sum: " + array.Sum());
+
+        array.Remove(12);
+        Console.WriteLine("Num silinenden sonra array: " + array.ToString());
+
+        array.RemoveRange(3, 9, 7);
+        Console.WriteLine("Nums range silinenden sonra array: " + array.ToString());
+    }
+}
+
